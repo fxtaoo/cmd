1239538d1ac18d0c39691860f84107f16dbf5df7
Fang Xuetao <fxtaoo@gmail.com>
2022-01-18 17:11:02 +0800

    替换为链接脚本

--- sys/change-user-passwd.sh
+++ sys/change-user-passwd.sh
@@ -3,30 +3,9 @@
 # $1 用户未指定，默认执行脚本用户
 # $2 用户未指定，生成 10 位随机密码
 
-function create_passwd(){
-  local num=$1
-  local passwd
-  if (( ${num:=10} < 4 )) ; then
-    echo "密码位数至少为 4 位，数字 大写字符 标点符号"
-    exit 1
-  fi
-  # 循环生成密码，直至生成符合要求密码
-  while : ; do
-    # tr -d 删除指定集 -c 反转
-    passwd="$(strings /dev/urandom | tr -dc '[:print:]'  | head -c$num)"
-    # 密码强度验证
-    # 必须含有 数字 大小写字符 标点符号
-    if [[ $(echo $passwd | sed 's/[[:digit:]]//') != "$passwd" && \
-    $(echo $passwd | sed 's/[[:lower:]]//') != "$passwd" && \
-    $(echo $passwd | sed 's/[[:upper:]]//') != "$passwd" && \
-    $(echo $passwd | sed 's/[[:punct:]]//') != "$passwd" && \
-    # 排除难终端分辨字符出现如 空格 等
-    $(echo $passwd | sed "s/[ 0oO1Ilz29gb6\"'\`]//") == "$passwd" ]] ; then
-      break
-    fi
-  done
-  echo $passwd
-}
+# 导入生成密码函数
+curl -o /tmp/create-passwd.sh https://raw.fxtaoo.dev/fxtaoo/cmd/master/app/create-passwd.sh
+source "/tmp/create-passwd.sh"
 
 function change_user_passwd(){
   local user_name
@@ -40,6 +19,8 @@ function change_user_passwd(){
   if $new_passwd ; then
     new_passwd=$(create_passwd 10)
   fi
-  echo -e "$new_passwd\n$new_passwd" | passwd $user_name
+  # echo -e "$new_passwd\n$new_passwd" | passwd $user_name
   echo "$user_name 密码修改为：$new_passwd"
 }
+
+change_user_passwd $1 $2
