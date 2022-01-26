74833e9ff28add8fd130189647f17a240a7ef08d
Fang Xuetao <fxtaoo@gmail.com>
2022-01-26 17:37:14 +0800

    update

--- sys/change-user-passwd.sh
+++ sys/change-user-passwd.sh
@@ -2,6 +2,7 @@
 # 重置用户密码
 # $1 用户未指定，默认执行脚本用户
 # $2 用户未指定，生成 10 位随机密码
+# bash -c "$(curl -fsSL https://raw.fxtaoo.dev/fxtaoo/cmd/master/sys/change-user-passwd.sh)"
 
 function change_user_passwd(){
   local user_name
@@ -15,7 +16,7 @@ function change_user_passwd(){
   if $new_passwd ; then
     new_passwd=$(bash -c "$(curl -fsSL https://raw.fxtaoo.dev/fxtaoo/cmd/master/app/create-passwd.sh)")
   fi
-  # echo -e "$new_passwd\n$new_passwd" | passwd $user_name
+  echo -e "$new_passwd\n$new_passwd" | passwd $user_name
   echo "$user_name 密码修改为：$new_passwd"
 }
 
