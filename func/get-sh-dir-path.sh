b8822f4285f1463806146fb3b5adc50a807d1ecd
Fang Xuetao <fxtaoo@gmail.com>
2022-03-25 05:05:46 -0700

    dirname 实现

--- func/get-sh-dir-path.sh
+++ func/get-sh-dir-path.sh
@@ -2,14 +2,16 @@
 # 返回 sh 脚本所在目录绝对路径
 
 function get_sh_dir_path(){
-  local sh_dir_path=$0
-  if [[ "$sh_dir_path" =~ "/" ]] ; then
-    sh_dir_path="${sh_dir_path%/*}"
-    if [[ $(echo "$sh_dir_path" | cut -c1) != '/' ]] ; then
-      sh_dir_path="$(pwd)/$sh_dir_path"
-    fi
-  else
-    sh_dir_path="$(pwd)"
-  fi
-  echo $sh_dir_path
+  # local sh_dir_path=$0
+  # if [[ "$sh_dir_path" =~ "/" ]] ; then
+  #   sh_dir_path="${sh_dir_path%/*}"
+  #   if [[ $(echo "$sh_dir_path" | cut -c1) != '/' ]] ; then
+  #     sh_dir_path="$(pwd)/$sh_dir_path"
+  #   fi
+  # else
+  #   sh_dir_path="$(pwd)"
+  # fi
+  # echo $sh_dir_path
+  echo $(dirname $0)
 }
+
