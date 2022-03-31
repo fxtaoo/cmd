21162b2527b249478f02b3f64a58b1ef602a3202
Fang Xuetao <fxtaoo@gmail.com>
2022-03-31 22:26:06 +0800

    修复 README.md 无变更导致 Actions 失败

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 # 生成 READ.me 文件
 # 提交变更
 
-go run ./readme-build/main.go
+go run ./readmeBuild/main.go
 
-git config --global user.name  "GitHub Actions"
-git config --global user.email  "i@fxtaoo.dev"
-git add .
-git commit -m "生成 READ.me 文件"
+if [[ $(git status | wc -l) -gt 4 ]] ; then
+    git config --global user.name  "GitHub Actions"
+    git config --global user.name  "GitHub Actions"
+    git config --global user.email  "i@fxtaoo.dev"
+    git add .
+    git commit -m "生成 READ.me 文件"
+fi
