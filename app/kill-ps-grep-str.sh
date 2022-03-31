e8f5db13f01b6e5cd5a6b238777664653b6356b4
Fang Xuetao <fxtaoo@gmail.com>
2022-03-31 21:29:46 +0800

    完善参数为空情形

--- app/kill-ps-grep-str.sh
+++ app/kill-ps-grep-str.sh
@@ -3,10 +3,16 @@
 # $1 指定字符串进程
 # bash -c "$(curl -fsSL https://raw.fxtaoo.dev/fxtaoo/cmd/master/app/kill-ps-grep-str.sh)"
 
+grep_str=$1
+
+if [[ $grep_str == '' ]] ;then
+    read -rp "输入字符串将杀死相关进程：" str
+fi
+
 oldifs=$IFS
 IFS=$'\n'
-for e in $(ps aux | grep $1);do
-    if [[ $(echo $e | awk '{print $11}') == "$1" ]] ; then
+for e in $(ps aux | grep $grep_str);do
+    if [[ $(echo $e | awk '{print $11}') == "$grep_str" ]] ; then
         for e2 in $(echo $e | awk '{print $2}');do
             kill $e2
         done
