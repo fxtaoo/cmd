c0c9a0fc9570efb3dabcbf5e785aa708c3c43cfe
Fang Xuetao <fxtaoo@gmail.com>
2022-04-11 17:07:44 +0800

    更全的生成

--- other/new-gop.sh
+++ other/new-gop.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 # 创建序列 go 项目
 # 序列：当前目录 n，序列号为 n+1
-# $1 为文件名名
-# $2 注释说明
+# $1 目录名
+# $2 注释
 
-num=1
+if [ $# -lt 2 ] ; then
+    echo "请输入 \$1 目录名和 \$2 注释"
+    exit 1
+fi
 
-for file in ./*;do
-  if [[ -d $file ]] ; then
-    ((num++))
+dir_num=1
+
+for dir in ./*;do
+  if [[ -d $dir ]] ; then
+    ((dir_num++))
   fi
 done
 
-dir_name="$num-$1"
-mkdir ./$dir_name \
-&& echo -e "// $2\n" > ./$dir_name/main.go
+dir_name="$dir_num-$1"
 
-# vscode 存在则打开
-if which code ; then
-  code ./$dir_name/main.go
+
+(mkdir ./$dir_name && \
+cd ./$dir_name && \
+go mod init $dir_name && \
+echo -e "// $2\n\npackage main" > ./main.go && \
+echo -e "# $2" > ./README.md)
+
+if [ $? -eq 0 ] ; then
+    echo -e "[$dir_num-$2](./$dir_name/README.md)" >> README.md
 fi
-
