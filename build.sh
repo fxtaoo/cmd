0bbc1cca17a74356686f8cba548421bddf652ead
Fang Xuetao <fxtaoo@gmail.com>
2022-04-13 16:39:14 +0800

    README.md 更改为执行文件生成

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# 生成 READ.me 文件
+# 生成 README.md 文件
 # 提交变更
 
-go run ./readmeBuild/main.go
+./readmebuild
 
 if [[ $(git status | wc -l) -gt 4 ]] ; then
     git config --global user.name  "GitHub Actions"
     git config --global user.name  "GitHub Actions"
     git config --global user.email  "i@fxtaoo.dev"
     git add .
-    git commit -m "生成 READ.me 文件"
+    git commit -m "生成 README.md 文件"
 fi
