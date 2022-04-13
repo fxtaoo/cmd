f6df8f356b820e93dc72cf1abeb48da93a364f85
Fang Xuetao <fxtaoo@gmail.com>
2022-04-13 10:38:09 +0800

    安装后添加到环境变量

--- install/golang.sh
+++ install/golang.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # golang 指定版本重装（linux-amd64，科大源）
-# $1 指定 golang 版本号，eg:1.17.6
-# $1 缺省 1.17.6
+# $1 指定 golang 版本号，eg:1.18.1
+# $1 缺省 1.18.1
 # bash -c "$(curl -fsSL https://raw.fxtaoo.dev/fxtaoo/cmd/master/install/golang.sh)"
 
 # 手动指定版本
@@ -9,7 +9,7 @@ if [[ $1 ]] ; then
  golang_version=$1
 fi
 
-golang_file_path="/tmp/go${golang_version:=1.17.6}.linux-amd64.tar.gz"
+golang_file_path="/tmp/go${golang_version:=1.18.1}.linux-amd64.tar.gz"
 
 wget http://mirrors.ustc.edu.cn/golang/go${golang_version}.linux-amd64.tar.gz -O $golang_file_path
 
@@ -25,6 +25,9 @@ fi
 # 国内使用七牛 Go 模块代理
 # https://github.com/goproxy/goproxy.cn/blob/master/README.zh-CN.md
 if ! ping -c 2 google.com ; then
-  go env -w GO111MODULE=on
-  go env -w GOPROXY=https://goproxy.cn,direct
+  (
+    PATH="$PATH:/usr/local/go/bin:"
+    go env -w GO111MODULE=on
+    go env -w GOPROXY=https://goproxy.cn,direct
+  )
 fi
