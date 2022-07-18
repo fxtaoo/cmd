454a60d08c4780d17534e5fa7685882a0cc45910
Fang Xuetao <fxtaoo@gmail.com>
2022-07-19 02:41:16 +0800

    自动获取最新版本

--- install/golang.sh
+++ install/golang.sh
@@ -1,19 +1,62 @@
 #!/usr/bin/env bash
-# golang 安装
-# $1 指定 go 版本号，eg:1.18.4,缺省 1.18.4
-# $2 指定架构，缺省 linux-amd64
+# golang 安装最新版本
+# $1 指定 go 版本号（eg:go1.18.4），缺省最新
+# $2 指定架构，缺省尝试自动获取
 # bash -c "$(curl -fsSL https://raw.fxtaoo.dev/fxtaoo/cmd/master/install/golang.sh)"
 # bash -c "$(wget -qO - https://raw.fxtaoo.dev/fxtaoo/cmd/master/install/golang.sh)"
 
 set -e
 
-go_version=$1
-cpu_type=$2
+download_site='https://go.dev/dl'
+golang_version_api='https://go.dev'
+if ! ping -c 1 google.com;then
+    download_site='https://mirrors.aliyun.com/golang'
+    golang_version_api='https://go.fxtaoo.dev'
+fi
 
-go_file_name="go${go_version:=1.18.4}.${cpu_type:=linux-amd64}.tar.gz"
+go_version=$1
+type=$2
+
+
+if [[ -z $go_version ]];then
+ go_version=$(curl -fsSL ${golang_version_api}/VERSION?m=text)
+fi
+
+# golang 以安装，比较版本号
+if which go >/dev/null 2>&1;then
+  v1=${go_version#*.}
+  v2=$(go version | awk '{print $3}')
+  v2=${v2#*.}
+  # v1 不大于 v2 结果 0，if 0 为真
+  if awk -v v1=$v1 -v v2=$v2 'BEGIN{exit v1>v2}' ; then
+    echo -e "当前 golang 版本 $v2 为最新\n"
+    exit 0
+  fi
+fi
+
+
+if [[ -z "$type" ]]; then
+  case $(uname -s) in
+    Linux)
+      type=linux ;;
+    *)
+      echo "不支持 OS: $(uname -s)"
+      exit 1 ;;
+  esac
+
+  case $(uname -m) in
+    x86_64)
+      type="${type}-amd64";;
+    *)
+      echo "不支持 arch: $(uname -m)"
+      exit 1 ;;
+  esac
+fi
+
+go_file_name="${go_version}.${type}.tar.gz"
 go_file_path="/tmp/${go_file_name}"
 
-wget https://mirrors.aliyun.com/golang/${go_file_name} -O $go_file_path
+wget ${download_site}/${go_file_name} -O $go_file_path
 
 if [[ -e $go_file_path ]] ; then
   sudo rm -rf /usr/local/go && \
@@ -22,10 +65,11 @@ if [[ -e $go_file_path ]] ; then
 fi
 
 # 七牛代理
-if ! ping -c 1 google.com ; then
+if [[ $golang_version_api == 'https://go.fxtaoo.dev' ]] ; then
   (
     PATH="$PATH:/usr/local/go/bin"
     go env -w GO111MODULE=on
     go env -w GOPROXY=https://goproxy.cn,direct
   )
 fi
+
