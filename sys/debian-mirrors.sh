70aeec454a98719200f56d703efc9d21e6848984
Fang Xuetao <fxtaoo@gmail.com>
2022-07-20 16:46:06 +0800

    fix bug

--- sys/debian-mirrors.sh
+++ sys/debian-mirrors.sh
@@ -8,7 +8,7 @@ set -e
 sudo apt install -y lsb_release
 
 cp /etc/apt/sources.list /etc/apt/sources.list.b
-sudo tee /etc/apt/sources.list.t <<< "deb https://mirrors.cloud.tencent.com/debian/ $(lsb_release -cs) main contrib non-free
+sudo tee /etc/apt/sources.list <<< "deb https://mirrors.cloud.tencent.com/debian/ $(lsb_release -cs) main contrib non-free
 deb https://mirrors.cloud.tencent.com/debian/ $(lsb_release -cs)-updates main contrib non-free
 deb https://mirrors.cloud.tencent.com/debian/ $(lsb_release -cs)-backports main contrib non-free
 deb https://mirrors.cloud.tencent.com/debian-security $(lsb_release -cs)-security main contrib non-free"
