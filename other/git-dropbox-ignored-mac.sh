13197e042f0ad1368b2239e5ba9dc25c9d4ad02d
Fang Xuetao <fxtaoo@gmail.com>
2023-05-17 21:05:21 +0800

    Dropbox 忽略 git clone 文件夹

---
+++ other/git-dropbox-ignored-mac.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Dropbox 忽略 git clone 文件夹
+# $1 git clone 路径
+# https://help.dropbox.com/zh-cn/sync/ignored-files
+# bash -c "$(curl -fsSL https://proxy.fxtaoo.dev/raw/fxtaoo/cmd/master/other/git-dropbox-ignored-mac.sh)"
+# bash -c "$(wget -qO - https://proxy.fxtaoo.dev/raw/fxtaoo/cmd/master/other/git-dropbox-ignored-mac.sh)"
+
+set -e
+
+git_url=$1
+dir_name=${git_url##*/}
+dir_name=${dir_name%.*}
+
+mkdir $dir_name
+xattr -w com.dropbox.ignored 1 ./$dir_name
+git clone $git_url
+
+
+
+
