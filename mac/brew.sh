4c70828adbb12259cd601ae08c46890fffc6145e
Fang Xuetao <fxtaoo@gmail.com>
2023-07-13 15:45:29 +0800

    fix bug

--- mac/brew.sh
+++ mac/brew.sh
@@ -1,31 +1,42 @@
 #!/usr/bin/env bash
 # 记录 macOS 中 brew 应用
 # 环境变量 $BREW_LIST_PATH 指定 brew-list 路径，缺省在 brew.sh 同目录中
+# 注意使用 gnu-sed
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/fxtaoo/cmd/master/ma c/brew.sh)"
 # bash -c "$(curl -fsSL https://proxy.fxtaoo.dev/cmd/mac/brew.sh)"
 
 set -e
 
+all_p=$*
+cmd="${all_p%% *}"
+apps="${all_p#* }"
+
 if [[ ${BREW_LIST_PATH} == "" ]];then
     BREW_LIST_PATH="$(dirname ${BASH_SOURCE[0]})/brew-list"
 fi
 
-p_2=$(echo "$*" | awk '{print $1}')
-
-if eval brew "$*";then
-    # 执行 brew 命令成功后修改
-    p_3=$(echo "$*" | awk '{print $2}')
-    tmpfile=$(mktemp)
-    case $p_2 in
-        install)
-            echo "$p_3" >> $BREW_LIST_PATH
-            cat $BREW_LIST_PATH > $tmpfile
-        ;;
-        uninstall)
-            grep -vw $p_3 $BREW_LIST_PATH > $tmpfile
-        ;;
-        *)
-            exit 0
-    esac
-    sort $tmpfile > $BREW_LIST_PATH
-fi
+case $cmd in
+    install)
+        for app in $apps;do
+            # 字符串不为 -- 开头，且安装成功
+            # "${string:0:2}" == "--"
+            if [[ ! $app =~ ^--.* ]] && brew install $app;then
+                echo "$app" >> $BREW_LIST_PATH
+            fi
+        done
+        tmpfile=$(mktemp)
+        sort $BREW_LIST_PATH > $tmpfile
+        mv $tmpfile $BREW_LIST_PATH
+    ;;
+    uninstall)
+         for app in $apps;do
+            # 字符串不为 -- 开头，且卸载成功
+            if [[ ! $app =~ ^--.* ]] && brew uninstall $app;then
+                 /opt/homebrew/opt/gnu-sed/libexec/gnubin/sed -i "/$app/d" $BREW_LIST_PATH
+            fi
+        done
+    ;;
+    *)
+        # 执行失败，脚本停止，执行成功脚本退出
+        brew "$*"
+esac
