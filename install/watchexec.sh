e83045fe4fe132d09dcc2d0e93d135063114a8af
Fang Xuetao <fxtaoo@gmail.com>
2023-07-23 11:22:04 +0800

    本地版本为最新不操作

--- install/watchexec.sh
+++ install/watchexec.sh
@@ -4,11 +4,13 @@
 # bash -c "$(curl -fsSL https://raw.githubusercontent.com/fxtaoo/cmd/master/install/watchexec.sh)"
 # bash -c "$(curl -fsSL https://proxy.fxtaoo.dev/cmd/install/watchexec.sh)"
 
-set -eu
+local_version=$(watchexec --version | grep release | awk '{print $2}')
+latest_version=$(curl --silent "https://api.github.com/repos/watchexec/watchexec/releases/latest" | jq -r .tag_name | sed 's/^.//')
 
-version=$(curl --silent "https://api.github.com/repos/watchexec/watchexec/releases/latest" | jq -r .tag_name | sed 's/^.//')
-curl -SL https://github.com/watchexec/watchexec/releases/download/v${version}/watchexec-${version}-x86_64-unknown-linux-gnu.tar.xz -o /tmp/watchexec-${version}-x86_64-unknown-linux-gnu.tar.xz
+if [[ "$local_version" != "$latest_version" ]];then
+    curl -SL https://github.com/watchexec/watchexec/releases/download/v${latest_version}/watchexec-${latest_version}-x86_64-unknown-linux-gnu.tar.xz -o /tmp/watchexec-${latest_version}-x86_64-unknown-linux-gnu.tar.xz
 
-tar -xf /tmp/watchexec-${version}-x86_64-unknown-linux-gnu.tar.xz -O watchexec-${version}-x86_64-unknown-linux-gnu/watchexec | sudo tee /usr/local/bin/watchexec > /dev/null
+    tar -xf /tmp/watchexec-${latest_version}-x86_64-unknown-linux-gnu.tar.xz -O watchexec-${latest_version}-x86_64-unknown-linux-gnu/watchexec | sudo tee /usr/local/bin/watchexec > /dev/null
 
-sudo chmod +x /usr/local/bin/watchexec
+    sudo chmod +x /usr/local/bin/watchexec
+fi
